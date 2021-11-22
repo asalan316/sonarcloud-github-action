6ceff96a1a02f0b3f84d32413d8501c84aae6eaa
Arsalan Khan <muhammad.arsalan.khan@sap.com>
2021-11-22 15:18:26 +0100

    use sonar scanner cli configurable

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,16 +6,19 @@ if [[ -z "${SONAR_TOKEN}" ]]; then
   echo "Set the SONAR_TOKEN env variable."
   exit 1
 fi
+# if USE_SONAR_SCANNER_CLI is set, use sonar scanner cli
+if [[ -z "${USE_SONAR_SCANNER_CLI}" ]]; then
+  echo "USE_SONAR_SCANNER_CLI env variable was not found... using pom.xml or build.gradle."
+   if [[ -f "pom.xml" ]]; then
+    echo "Maven project detected. You should run the goal 'org.sonarsource.scanner.maven:sonar' during build rather than using this GitHub Action."
+    exit 1
+   fi
 
-# if [[ -f "pom.xml" ]]; then
-#  echo "Maven project detected. You should run the goal 'org.sonarsource.scanner.maven:sonar' during build rather than using this GitHub Action."
-#  exit 1
-# fi
-
-# if [[ -f "build.gradle" ]]; then
-#  echo "Gradle project detected. You should use the SonarQube plugin for Gradle during build rather than using this GitHub Action."
-#  exit 1
-# fi
+   if [[ -f "build.gradle" ]]; then
+    echo "Gradle project detected. You should use the SonarQube plugin for Gradle during build rather than using this GitHub Action."
+    exit 1
+   fi
+fi
 
 if [[ -z "${SONARCLOUD_URL}" ]]; then
   SONARCLOUD_URL="https://sonarcloud.io"
